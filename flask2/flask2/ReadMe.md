4a5d1a1564f4611afac72a6fbe528c2a1d725eb4
edvards-klavins <56105785+edvards-klavins@users.noreply.github.com>
2020-01-28 11:28:11 +0200

    Add files via upload

---
+++ flask2/flask2/ReadMe.md
@@ -0,0 +1,1 @@
+Mans web servers.
---
+++ flask2/main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def getIndex():
+  return "Hi!"
+
+
+@app.route('/home')
+def getHome():
+  return render_template('home.html', active_page = 'home')
+
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+  return render_template('contact.html', phone = "123")
+
+
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", threaded=True, port=5050, debug=True) 
